19a1f13c3481de5c86f6719d6926eb4c9c909cad
shajiofisgodgift <shajiofisigift@gmail.com>
2025-05-16 13:27:52 +0100

    add boiler plate

---
+++ main.js
@@ -0,0 +1,8 @@
+//alert("gift")
+let a = 5
+let b = 6
+let answer = a+b
+//console.lo
+
+
+
---
+++ oprators.js
@@ -0,0 +1,61 @@
+let number1 = 3
+let number2 = 5
+let results;
+// subtraaction
+results=number1-number2
+console.log(results)
+
+let number3 = 3
+let number4 = 5
+// multiplication
+results=number3*number4
+console.log(results)
+
+let number6 = 3
+let number7 = 5
+// divsion
+results=number6/number7
+console.log(results)
+
+let number8 = 5
+let number9 = 2
+// module
+results=number8%number9
+console.log(results)
+
+let x=5
+x+=2;
+console.log(x)
+
+y=5
+y%=2
+console.log(y)
+
+u=5
+u/=4
+console.log(u)
+
+t=5
+t*=2
+console.log(t)
+
+j=5
+j-=2
+console.log(j)
+
+console.log(5=="5")
+console.log(5==="5")
+console.log(5!="5")
+console.log(5!=="5")
+
+console.log(5>3)
+console.log(5<3)
+console.log(5<=5)
+console.log(5>=5)
+
+let m=5
+let c= 3
+let admin = true
+console.log(m>0 && c>0)
+console.log(m<0 || c<0)
+console.log(!admin)
